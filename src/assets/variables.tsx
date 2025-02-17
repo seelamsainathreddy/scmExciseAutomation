db0d6d48ca2a1ec79cf8529a8aed93b95dd33751
sainathreddyseelam <ism2015006@iiita.ac.in>
2025-02-17 12:21:16 +0530

    variable change

--- src/assets/variables.tsx
+++ src/assets/variables.tsx
@@ -1,7 +1,7 @@
 // const backendURL1 = "https://scmgovtexcisebackend-production.up.railway.app:8080";
-//const backendURL = "https://scmgovtexcisebackend.onrender.com";
+const backendURL = "https://scmgovtexcisebackend.onrender.com";
 
-const backendURL = "http://127.0.01:5001";
+// const backendURL = "http://127.0.01:5001";
 
 
 
